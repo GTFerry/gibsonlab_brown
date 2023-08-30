b21afb40fcfd0f2313a55fbd2cfd1c575ceba020
Ferry <alessandro.ferrari.2004@gmail.com>
2023-08-30 11:55:02 -0400

    plot new

--- ari-merging-report-script.R
+++ ari-merging-report-script.R
@@ -24,6 +24,7 @@ sumdata$key=rownames(sumdata)
 
 # save plots
 pdf(file = "RandIndex_heatmap_report.pdf", height = 15, width = 15)
+plot.new()
 title("Plot Before ARI Merging (Dune)")
 plotARIs(clusMat = merger) + RotatedAxis()
 
