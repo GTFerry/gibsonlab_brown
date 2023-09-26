075dba4a924487133fa3251684aa588667a0b35f
Ferry <alessandro.ferrari.2004@gmail.com>
2023-09-26 15:54:14 -0400

    added full script for pseudo and calling

--- src/peak-calling.R
+++ src/peak-calling.R
@@ -1,3 +1,27 @@
 library(ArchR)
 
-archR_project <- addGroupCoverages(ArchRProj = archR_project, groupBy = "Cluster_1_ResRNA_0.75_ResATAC_0.75_Iters_3_Dims_1to19")
+archR_project <- loadArchRProject("saves/labeled")
+
+print("Loaded Project. Starting Pseudo Bulk...")
+
+archR_project <- addGroupCoverages(ArchRProj = archR_project, groupBy = "SeuratCluster")
+
+print("Pseudo Bulk completed. Saving...")
+
+saveArchRProject(archR_project, "saves/pseudobulked", load = FALSE, overwrite = TRUE)
+
+print("Starting Peak calling...")
+
+pathToMacs2 <- findMacs2()
+
+archR_project <- addReproduciblePeakSet(
+  ArchRProj = archR_project, 
+  groupBy = "SeuratCluster", 
+  pathToMacs2 = pathToMacs2
+)
+
+getPeakSet(archR_project)
+
+print("Peak Calling Finished. Saving...")
+
+saveArchRProject(archR_project, "saves/peak-called", load = FALSE, overwrite = TRUE)
